93cdb890b3c8f695d098e053a941a7a4749d48d9
Ludy87 <Ludy87@users.noreply.github.com>
2024-09-19 09:41:43 +0200

    init

---
+++ src/main/java/stirling/software/SPDF/utils/RequestUriUtils.java
@@ -0,0 +1,23 @@
+package stirling.software.SPDF.utils;
+
+public class RequestUriUtils {
+
+    public static boolean isStaticResource(String requestURI) {
+
+        return isStaticResource("", requestURI);
+    }
+
+    public static boolean isStaticResource(String contextPath, String requestURI) {
+
+        return requestURI.startsWith(contextPath + "/css/")
+                || requestURI.startsWith(contextPath + "/fonts/")
+                || requestURI.startsWith(contextPath + "/js/")
+                || requestURI.startsWith(contextPath + "/images/")
+                || requestURI.startsWith(contextPath + "/public/")
+                || requestURI.startsWith(contextPath + "/pdfjs/")
+                || requestURI.startsWith(contextPath + "/saml2")
+                || requestURI.endsWith(".svg")
+                || requestURI.endsWith(".webmanifest")
+                || requestURI.startsWith(contextPath + "/api/v1/info/status");
+    }
+}
